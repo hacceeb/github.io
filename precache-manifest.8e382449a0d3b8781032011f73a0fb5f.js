5c3e77439111d1a5da8a54687ad5507fb31011b2
hacceeb <hacceebhassan@users.noreply.github.com>
2020-09-04 01:00:16 +0100

    Updates

---
+++ precache-manifest.8e382449a0d3b8781032011f73a0fb5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52232be7a47dbd6774bd75901b8509de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4be2aed3e86c9c8400bf",
+    "url": "./static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fa1e292ecdefad31d5b6",
+    "url": "./static/js/2.e0c77d0f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "./static/js/2.e0c77d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be2aed3e86c9c8400bf",
+    "url": "./static/js/main.53ac7ca7.chunk.js"
+  },
+  {
+    "revision": "64185b32628508d024a5",
+    "url": "./static/js/runtime-main.5e4f239c.js"
+  }
+]);
